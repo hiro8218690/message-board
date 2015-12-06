3c0c9cd7d4feeddff52113385120d87febb59459
hiro8218690 <hiro8218690@gmail.com>
2015-12-06 09:31:06 +0000

    recorrection

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,5 @@
 class Message < ActiveRecord::Base
+    #名前は必須　かつ　20文字以内
+    validates :name , length: { maximum: 20 } , presence: true
+    validates :body , length: { minimum: 2, maximum: 30 } , presence: true
 end
